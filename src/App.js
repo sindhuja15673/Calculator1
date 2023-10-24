546969ed0732fefb40f93d877d03b5436e0f33d9
sindhuja15673 <132433742+sindhuja15673@users.noreply.github.com>
2023-10-24 06:23:49 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [display, setDisplay] = useState(0);
+  const [currentOperand, setCurrentOperand] = useState('')
+  // const [previousOperand, setPreviousOperand] = useState('');
+  const [operator, setOperator] = useState('');
+
+  const handleOperandClick =(digit)=>{
+    if(display==='0'|| operator){
+      setDisplay(digit);
+    }
+    else{
+      setDisplay(Number(display+digit));
+    }
+  }
+
+  const handleOperatorClick =(selectedOperator) =>{
+    if(!operator){
+      setCurrentOperand(display);
+      setOperator(selectedOperator);
+    }
+  }
+
+  const handleEqualClick = ()=>{
+    console.log(`${currentOperand} ${operator} ${display}`);
+    
+    setDisplay(String(eval(`${currentOperand} ${operator} ${display}`)));
+    setCurrentOperand('');
+    setOperator('');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='cal'>
+    <div className='background-image'></div>
+    <div className='calculator'>Calculator
+      <div className='display'>{display}</div>
+      <div className='buttons'>
+        {
+          [7,8,9,'+',4,5,6,'-',1,2,3,'*',0,'=','/'].map(item =>{
+            return <button key={item}
+            onClick={()=>{
+              if (typeof item === 'number'){
+                handleOperandClick(String(item));
+              }
+              else if(item === '='){
+                handleEqualClick();
+              }
+              else{
+                handleOperatorClick(item);
+              }
+            }}
+            >{item}</button>
+            
+          })
+        }
+        <button onClick={()=>{setDisplay('0'); setCurrentOperand('')}}>AC</button>
+      </div>
+      
+    </div>
     </div>
   );
 }
